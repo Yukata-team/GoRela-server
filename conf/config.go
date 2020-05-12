7fc8eaa6d45d34baf67d622cc3dcb35ee332693e
Yugo <yu1931sn@ezweb.ne.jp>
2020-05-12 18:53:20 +0900

    [Update] configファイル

--- conf/config.go
+++ conf/config.go
@@ -3,6 +3,7 @@ package conf
 const (
 	USER     string = "root"
 	PASS     string = "root"
-	PROTOCOL string = "tcp(localhost:3306)"
+	HOST 	 string = "docker_mysql_mysql_1"
+	PORT 	 string = "3306"
 	DBNAME   string = "gorela_db"
 )
--- model/mysql.go
+++ model/mysql.go
@@ -12,11 +12,7 @@ var db *gorm.DB
 
 //DB接続
 func Init() *gorm.DB {
-	fmt.Println(conf.USER)
-	fmt.Println(conf.PASS)
-	fmt.Println(conf.PROTOCOL)
-	fmt.Println(conf.DBNAME)
-	CONNECT := conf.USER + ":" + conf.PASS + "@" + conf.PROTOCOL + "/" + conf.DBNAME + "?charset=utf8mb4&parseTime=true&loc=Asia%2FTokyo"
+	CONNECT := conf.USER + ":" + conf.PASS + "@" + "tcp(" + conf.HOST + ":" + conf.PORT + ")" + "/" + conf.DBNAME + "?charset=utf8mb4&parseTime=true&loc=Asia%2FTokyo"
 	db, err := gorm.Open("mysql", CONNECT)
 	if err != nil {
 		panic(err.Error())
