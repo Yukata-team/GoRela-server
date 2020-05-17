9d356c012b322b1f29f3e3733142c5db7f8ee24d
Yugo <yu1931sn@ezweb.ne.jp>
2020-05-17 12:04:24 +0900

    [Update] ddl.sql

--- db/init/ddl.sql
+++ db/init/ddl.sql
@@ -4,7 +4,7 @@ USE gorela_db;
 CREATE TABLE `gorela_db`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(128) NOT NULL,
   `introduction` VARCHAR(128) NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp,
   `updated_at` TIMESTAMP NOT NULL DEFAULT current_timestamp on update current_timestamp,
