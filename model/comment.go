7bda35e5e9e857989f70c8342994b0273803e590
Yugo <yu1931sn@ezweb.ne.jp>
2020-05-23 20:10:21 +0900

    [Update] commentの作成日を返すように変更

--- model/comment.go
+++ model/comment.go
@@ -1,11 +1,15 @@
 package model
 
+import "time"
+
 type Comment struct {
 	ID int `json:"id" gorm:"primaly_key"`
 	PostId int `json:"post_id"`
 	UserId int `json:"user_id"`
 	Content string `json:"content"`
 	User User `json:"user" gorm:"foreignkey:UserId"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type Comments []Comment
